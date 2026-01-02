655a0fa27e1922554021f668a9e7bf30a551b654
Alex <agscheaua@gmail.com>
2026-01-02 19:31:23 +0200

    game logic is lacking
    i need to work on the logic that make a new grid

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
 
-const container = document.querySelector(".container");
+let container = document.querySelector(".container");
 
-for (i = 1; i <= 256; i++) {
+function getNewGrid () { 
+
+let nrSqareSide = Number(prompt("Insert the number of sqares that you want on all sildes of the grid!")); 
+let nrSqareTotal = nrSqareSide * nrSqareSide;
+let sqareSize = 900 / nrSqareSide; 
+
+for (let i = 1; i <= nrSqareTotal; i++) {
     let div = document.createElement("div");
-    div.classList.add("sqare");
-    container.appendChild(div);
-}
+    div.classList.add("sqare"); 
+    div.style.width = `${sqareSize}px`;
+    div.style.height = `${sqareSize}px`; 
+    container.appendChild(div); 
+};
 
 let sqares = document.querySelectorAll(".sqare");
+
+sqares.forEach( (sqare) => {
+    if ( sqare.offsetWidth !== sqareSize ) {
+        sqare.remove();  
+    }
+    else if ( (sqare.offsetWidth === sqareSize) &&
+    (sqares.length !== nrSqareTotal) ) {
+        for (let i = 0; i <= nrSqareTotal - 1; i++) {
+            sqares[i].remove(); 
+            console.log("removed");      
+        };  
+    }   
+    else {  
+        console.log("done");
+    };
+} ); 
+   
 sqares.forEach( (sqare) => {
     sqare.addEventListener("mouseenter", () => {
         sqare.style.backgroundColor = "purple";
-    });
-} ); 
+    } );
+} );  
+ 
+let resetGrid = document.querySelector(".resetGrid");
+resetGrid.addEventListener("click", () => {
+    sqares.forEach( (sqare) => {
+        sqare.style.backgroundColor = "red";
+    } ); 
+} );                
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-    let nrSqareSide = Number(prompt("Insert the number of sqares you want: "));
-    let sqareSize = 1600 / nrSqareSide;
-    console.log(sqareSize);    
-}); 
+
+}; //function getNewGrid END. 
+
+let startGrid = document.querySelector(".startGrid");
+startGrid.addEventListener("click", getNewGrid);  
+
+
