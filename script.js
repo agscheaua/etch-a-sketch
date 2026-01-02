4b5af71ada17900b2244d21af4ef216f5eba7f0b
Alex <agscheaua@gmail.com>
2026-01-02 22:47:53 +0200

    game logic fixed,
    the saqres from the last iteration will be deleted
    at the begenning of the next one

--- script.js
+++ script.js
@@ -2,54 +2,56 @@
 let container = document.querySelector(".container");
 
 function getNewGrid () { 
-
-let nrSqareSide = Number(prompt("Insert the number of sqares that you want on all sildes of the grid!")); 
-let nrSqareTotal = nrSqareSide * nrSqareSide;
-let sqareSize = 900 / nrSqareSide; 
-
-for (let i = 1; i <= nrSqareTotal; i++) {
-    let div = document.createElement("div");
-    div.classList.add("sqare"); 
-    div.style.width = `${sqareSize}px`;
-    div.style.height = `${sqareSize}px`; 
-    container.appendChild(div); 
-};
-
-let sqares = document.querySelectorAll(".sqare");
-
-sqares.forEach( (sqare) => {
-    if ( sqare.offsetWidth !== sqareSize ) {
-        sqare.remove();  
-    }
-    else if ( (sqare.offsetWidth === sqareSize) &&
-    (sqares.length !== nrSqareTotal) ) {
-        for (let i = 0; i <= nrSqareTotal - 1; i++) {
-            sqares[i].remove(); 
-            console.log("removed");      
-        };  
-    }   
-    else {  
-        console.log("done");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     };
-} ); 
-   
-sqares.forEach( (sqare) => {
-    sqare.addEventListener("mouseenter", () => {
-        sqare.style.backgroundColor = "purple";
-    } );
-} );  
+    
+    let nrSqareSide = prompt("Insert the number of sqares that you want on all sildes of the grid!"); 
+    if (nrSqareSide === "" || nrSqareSide === null) {
+        console.log("insert a valid number");
+    } 
+    else if (nrSqareSide % nrSqareSide !== 0) {
+        console.log("insert a valid nr."); 
+    } 
+    else {}; 
+
+    let nrSqareTotal = nrSqareSide * nrSqareSide;
+    let sqareSize = 900 / nrSqareSide;  
  
-let resetGrid = document.querySelector(".resetGrid");
-resetGrid.addEventListener("click", () => {
+    for (let i = 1; i <= nrSqareTotal; i++) {
+        let div = document.createElement("div");
+        div.classList.add("sqare"); 
+        div.style.width = `${sqareSize}px`; 
+        div.style.height = `${sqareSize}px`; 
+        container.appendChild(div);   
+    };
+ 
+    let sqares = document.querySelectorAll(".sqare"); 
+ 
     sqares.forEach( (sqare) => {
-        sqare.style.backgroundColor = "red";
+        if ( sqare.offsetWidth !== sqareSize ) {
+            sqare.remove();     
+        }    
+        else {};
     } ); 
-} );                
-
-
+   
+    sqares.forEach( (sqare) => {
+        sqare.addEventListener("mouseenter", () => {
+            sqare.style.backgroundColor = "purple";
+        } );
+    } );  
+ 
+    let resetGrid = document.querySelector(".resetGrid");
+    resetGrid.addEventListener("click", () => {
+        sqares.forEach( (sqare) => {
+            sqare.style.backgroundColor = "red";
+        } ); 
+    } );    
+    
 }; //function getNewGrid END. 
 
-let startGrid = document.querySelector(".startGrid");
-startGrid.addEventListener("click", getNewGrid);  
+let newGrid = document.querySelector(".newGrid"); 
+newGrid.addEventListener("click", getNewGrid);  
 
 
+ 
